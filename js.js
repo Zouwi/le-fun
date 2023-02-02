e690b35ae7173094e06c245aea0a779aa9be4e02
Zouwi <zoegabet89@gmail.com>
2023-02-02 13:40:39 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,11 @@
+// myAudioElement.addEventListener("canplaythrough", (event) => {
+//     /* the audio is now playable; play it if permissions allow */
+//      new Audio("https://youtu.be/YRvOePz2OqQ");
+//     myAudioElement.play();
+// });
+
+// var music = document.createElement("AUDIO");
+
+document.querySelector(".musicos").autoplay;
+
+
